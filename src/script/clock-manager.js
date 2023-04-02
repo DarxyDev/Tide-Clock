46ff5bff5c8243bc9281f86830d37ce7afc6c853
DarxyDev <damongarvin@gmail.com>
2023-04-02 18:32:07 -0400

    Basic mechanics seem to be working

--- src/script/clock-manager.js
+++ src/script/clock-manager.js
@@ -1,6 +1,6 @@
 const clockManager = (() => {
     let ref = {};
-    let tide = {};
+    let tideArr = [];
     let date = new Date();
     function setReferences(refObj) {
         ref = refObj;
@@ -20,34 +20,32 @@ const clockManager = (() => {
             let turns = min / (12 * 60);
             _rotateHand(ref.hour, turns);
         }
-        
+
     }
-    function setTideData(low1,high1,low2,high2){
-        tide.low1 = low1;
-        tide.high1 = high1;
-        tide.low2 = low2;
-        tide.high2 = high2;
+    function setTideData(tideArray) {
+        tideArr = tideArray;
     }
     function setTideHand() {
         //get current AM/PM tide times
         let min = ((date.getHours() % 12) * 60) + date.getMinutes();
-        
-        let flooding = true;
-        let prevTide =  2 * 60 + 36;
-        let nextTide = 10 * 60 + 40;
-
+        let i = 0;
+        while(tideArr[i].min < min) i++;
+        let prevTide = tideArr[i - 1].min;
+        let nextTide = tideArr[i].min;
+        let flooding = tideArr[i].type === 'HIGH';
+        console.log({prevTide,nextTide,flooding,})
 
         let tideCycle = Math.abs(nextTide - prevTide)
         min -= prevTide % tideCycle;
         let turns = (min / tideCycle) / 2;
-        if(flooding) turns += .5;
+        if (flooding) turns += .5;
         _rotateHand(ref.tide, turns);
     }
     function _rotateHand(hand, turns) {
         hand.style.transform = 'rotate(' + turns + 'turn)';
     }
 
-    return { setReferences, setTimeHands, setTideHand, setTideData};
+    return { setReferences, setTimeHands, setTideHand, setTideData };
 })()
 
 export default clockManager;
